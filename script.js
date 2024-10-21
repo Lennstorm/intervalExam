b68a32897870f37f492294e1827f44626e4f7936
Lennstorm <4824136@gmail.com>
2024-10-21 18:59:14 +0200

    layout klar

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     placeMarks();
+
+    function navigateTo(pageId) {
+        const allPages = dockument.querySelector('.page');
+
+        allPages.forEach(page => {
+            if (page.id === pageId) {
+                page.classList.remove('page--inactive');
+                page.classList.add('page--active');
+            } else {
+                page.classList.remove('page--active');
+                page.classList.add('page--inactive');
+            }
+        });
+    }
+
+    document.querySelectorAll('.menu__item').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetPage = e.target.getAttribute('onclick').split("'")[1]; //hämta pageId av onclick
+            navigateTo(targetPage);
+        });
+    });
+    document.querySelector('.landing-wrapper').addEventListener('click', () => navigateTo('setTimer'));
+    document.querySelector('.setTimer-btn').addEventListener('click', () => navigateTo('analog-timer'));
 });
 
 function placeMarks() {
@@ -11,7 +34,7 @@ function placeMarks() {
     for (let i = 0; i < 60; i++) {
         const mark = document.createElement('div');
         mark.classList.add('analog-timer__mark');
-        mark.style.transform = `rotate(${i * 6}deg) translateY(-1200%)`;
+        mark.style.transform = `rotate(${i * 6}deg) translateY(-1350%)`;
         clockFace.appendChild(mark);
     }
     console.log('marks placed')
